f9429931196d973567d6dc4b1f7bcfb79cfe1dff
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-10-13 16:29:23 -0300

    creating and getting orphanages

--- server/src/database/migrations/1602611003232-create_orphanages.ts
+++ server/src/database/migrations/1602611003232-create_orphanages.ts
@@ -1,13 +1,58 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class createOrphanages1602611003232 implements MigrationInterface {
+export default class createOrphanages1602611003232
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orphanages',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'latitude',
+            type: 'decimal',
+            precision: 12,
+            scale: 10,
+          },
+          {
+            name: 'longitude',
+            type: 'decimal',
+            precision: 12,
+            scale: 10,
+          },
+          {
+            name: 'about',
+            type: 'text',
+          },
+          {
+            name: 'instructions',
+            type: 'text',
+          },
+          {
+            name: 'opening_hours',
+            type: 'varchar',
+          },
+          {
+            name: 'open_on_weekends',
+            type: 'boolean',
+            default: false,
+          },
+        ],
+      }),
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-         
-    
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orphanages');
+  }
 }
---
+++ server/src/routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+import OrphanagesController from './controllers/OrphanagesController';
+
+const routes = Router();
+
+routes.post('/orphanages', OrphanagesController.create);
+routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
+
+export default routes;
