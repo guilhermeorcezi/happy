d55c65413de0c2314ca03212f413fe4e4375f2fe
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-10-13 14:53:32 -0300

    eslint and prettier configured

---
+++ server/src/database/migrations/1602611003232-create_orphanages.ts
@@ -0,0 +1,13 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export default class createOrphanages1602611003232 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+         
+    
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
---
+++ server/src/server.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+import './database/connection';
+
+const app = express();
+app.use(express.json());
+
+app.listen(3333);
